df73994cf1afb87415df98e739aa55ca4ceb6bc3
Ivaness Chiong <ivanesschiong@gmail.com>
2014-09-25 13:46:21 -0500

    Added project.clj

---
+++ project.clj
@@ -0,0 +1,4 @@
+(defproject linked-list "0.0.1-SNAPSHOT"
+  :description "The first lab for CS331 at IIT. Most of the code and the test cases are provided. Their task is to create 2 functions and test cases for them."
+  :dependencies [[org.clojure/clojure "1.4.0"]]
+  :profiles {:dev {:dependencies [[midje "1.5.0"]]}})
